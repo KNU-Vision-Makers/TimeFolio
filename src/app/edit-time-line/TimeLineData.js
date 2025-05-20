e236375a5724253aa553d461c1ecbb3d0d2b7aca
naldori <subin8509@gmail.com>
2025-05-20 17:10:23 +0900

    TimeLine Design First

---
+++ src/app/edit-time-line/TimeLineData.js
@@ -0,0 +1,46 @@
+export const timelineData = [
+  {
+    title: "University Admission",
+    subtitle: "Hanyang University",
+    date: "Mar 2021",
+    category: "education",
+  },
+  {
+    title: "Global Software Bootcamp",
+    subtitle: "Google Developers Korea",
+    date: "Jul 2022",
+    category: "program",
+  },
+  {
+    title: "Coding Mentor",
+    subtitle: "Youth Coding Bridge",
+    date: "Jan 2023",
+    category: "volunteer",
+  },
+  {
+    title: "Hackathon Victory",
+    subtitle: "Hanyang Innovation Hackathon",
+    date: "Aug 2023",
+    category: "award",
+  },
+  {
+    title: "Exchange Program",
+    subtitle: "TU Munich",
+    date: "Nov 2023",
+    category: "program",
+  },
+  {
+    title: "Research Poster",
+    subtitle: "Korea Computer Congress",
+    date: "Apr 2024",
+    category: "research",
+  },
+];
+
+export const categoryColorMap = {
+  education: "#ff6666",  // 붉은 계열
+  program: "#00bcd4",    // 청록 계열
+  volunteer: "#8bc34a",  // 연두 계열
+  award: "#ff9800",      // 주황 계열
+  research: "#9c27b0",   // 보라 계열
+};
--- src/app/edit-time-line/page.jsx
+++ src/app/edit-time-line/page.jsx
@@ -1,11 +1,46 @@
+'use client';
+
 import styles from './page.module.css';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { timelineData, categoryColorMap } from "./TimelineData";
 
 // 타임라인 수정 페이지
+// 수빈 작성
+
 const EditTimeLinePage = () => {
     return(
-        <>
-            ## 수빈양 파트 - 타임라인 수정
-        </>
+    <div className = {styles['content-box']}>
+      <h1 className={styles['content-title-middle']}>TIMELINE ABOUT ME</h1>
+      <VerticalTimeline>
+        {timelineData.map((item, index) => {
+        const color = categoryColorMap[item.category] || "#607d8b"; // fallback gray
+
+        return (
+          <VerticalTimelineElement
+            key={index}
+            className="vertical-timeline-element--work"
+            date={item.date}
+            contentStyle={{ border: `2px solid ${color}`, background: "#fff" }}
+            iconStyle={{
+                background: '#fff',
+                border: `5px solid ${color}`,
+                width: '20px',
+                boxShadow : 'none',
+                height: '20px',
+                marginLeft: '-10px',  // 가운데 정렬 조정 필요
+                marginTop: '21px'
+            }}
+            contentArrowStyle={{ borderRight: `48px solid ${color}` }}
+          >
+            <h3 className="vertical-timeline-element-title">{item.title}</h3>
+            <h4 className={styles['vertical-timeline-element-subtitle']}>HANYANG UNIVERSITY</h4>
+
+          </VerticalTimelineElement>
+        );
+      })}
+      </VerticalTimeline>
+    </div>
     )
 }
 
