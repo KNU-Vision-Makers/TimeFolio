1a1a1acb4fcb25c1e6e770a700035b6e9b19b9b9
EunjeongKim <kimeunjeong0821@gmail.com>
2025-05-22 02:04:26 +0900

    Feat: Sidebar 구현

---
+++ src/components/NamePlate.jsx
@@ -0,0 +1,11 @@
+import styles from "../styles/globals.css";
+
+const NamePlate = () => {
+    return(
+        <div className="name-plate">
+            Ryu<br/>Subin
+        </div>
+    )
+}
+
+export default NamePlate
