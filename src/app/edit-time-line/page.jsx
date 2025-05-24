4df22de11f0112b47faf6183f7f41a1a087706da
naldori <subin8509@gmail.com>
2025-05-25 01:21:13 +0900

    change-edit-text-area

--- src/app/edit-time-line/page.jsx
+++ src/app/edit-time-line/page.jsx
@@ -1,12 +1,17 @@
 'use client';
+import { useState } from 'react';
 import TimeLine from '@/components/TimeLine';
 import styles from './page.module.css';
 
-//타임라인 수정 페이지 - 수빈 작성
-const TimeLineEditButton = () => {
+const TimeLineEditButton = ({ editMode, setEditMode }) => {
   return (
     <div>
-      <button className={styles['time-line-edit-button']}>+ edit</button>
+      <button
+        className={styles['time-line-edit-button']}
+        onClick={() => setEditMode(prev => !prev)}
+      >
+        {editMode ? 'Done' : '+ edit'}
+      </button>
     </div>
   );
 };
@@ -14,10 +19,11 @@ const TimeLineEditButton = () => {
 
 
 const EditTimeLinePage = () => {
+  const [editMode, setEditMode] = useState(false);
   return (
     <div className="with-hover">
-      <TimeLine />
-      <TimeLineEditButton/>
+      <TimeLine editMode={editMode} setEditMode={setEditMode} />
+      <TimeLineEditButton editMode={editMode} setEditMode={setEditMode} />
     </div>
   );
 };
