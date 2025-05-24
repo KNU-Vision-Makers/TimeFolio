519321fb87600ffbc30eaee05bd9f34cb1d89f91
naldori <subin8509@gmail.com>
2025-05-25 00:36:07 +0900

    edit-button-added

--- src/app/edit-time-line/page.jsx
+++ src/app/edit-time-line/page.jsx
@@ -1,12 +1,23 @@
 'use client';
 import TimeLine from '@/components/TimeLine';
+import styles from './page.module.css';
 
 //타임라인 수정 페이지 - 수빈 작성
+const TimeLineEditButton = () => {
+  return (
+    <div>
+      <button className={styles['time-line-edit-button']}>+ edit</button>
+    </div>
+  );
+};
+
+
 
 const EditTimeLinePage = () => {
   return (
     <div className="with-hover">
       <TimeLine />
+      <TimeLineEditButton/>
     </div>
   );
 };
